32a89fe54c0c5f2bef5fb31b812ba6ecd7a09a95
Luiz Felipe Henrique Pereira <lfhenrique95@gmail.com>
2022-03-18 15:26:28 -0300

    Adding Java Script in Application

---
+++ assest/Js/script.js
@@ -0,0 +1,74 @@
+/*Declarações de variaveis
+var nome // escopo global
+ // escopo fechado(variavel que pode mudar ao longo do tempo)
+const nome //escopo fechado(variavel que não muda ao longo do tempo)
+/**/
+/*
+case Sensitive= reconhece letras maiusculas e minusculas
+por tag: getElementByTagName()
+por id:getElementById()
+por nome :getElementByClassName()
+por seletor:querySelector()
+*/
+
+let nome = window.document.getElementById("nome")
+let email = document.querySelector("#email")
+let assunto = document.querySelector("#assunto")
+
+nome.style.width = "100%"
+email.style.width= "100%"
+
+function validaNome () {
+  let txtNome = document.querySelector("#txtNome")                                                                                                                   
+if(nome.value.length< 3){
+txtNome.innerHTML = 'nome invalido'
+txtNome.style.color = 'red'
+} else {
+  txtNome.innerHTML = "Nome Válido"                                                                                                             
+  txtNome.style.color = 'green'
+}        
+}
+function validaEmail(){
+   let txtEmail = document.querySelector('#txtEmail')     
+
+   if (email.value.indexof('@') == -1|| email.value.indexof(".") == -1){
+   txtEmail.innerHTML = 'E-mail Invalido'
+   txtEmail.style.color = 'red'
+
+   }else{
+   txtEmail.innerHTML = 'E-mail Invalido'
+   txtEmail.style.color = 'green'
+   emailOk = true
+   }
+}
+
+function validaAssunto() {
+  let txtAssunto = document.querySelector('#txtAssunto')
+
+  if (assunto.value.length >= 100) {
+     txtAssunto.innerHTML = 'Conte-em Outras Palavras'
+     txtAssunto.style.color = 'red'
+     txtAssunto.style.display = 'block'
+  } else {
+     txtAssunto.style.display = 'none'
+     assuntoOk = true
+  }
+}
+
+function enviar() {
+  if (nomeOk == true && emailOk == true && assuntoOk == true) {
+     alert ('Obrigado Por Compartilhar!')
+  } else {
+     alert ('Excesso de Letras tente de outra forma ...')
+  }
+}
+
+function mapaZoom() {
+  mapa.style.width = '800px'
+  mapa.style.height = '600px'
+}
+
+function mapaNormal() {
+  mapa.style.width = '400px'
+  mapa.style.height = '250px'
+}
